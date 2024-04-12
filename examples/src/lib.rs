5aa433e2d982d8c2040b77c5bc1e375991a8decd
Henauxg <19689618+Henauxg@users.noreply.github.com>
2024-04-12 12:28:23 +0200

    examples(all): Move COLORS array to shared lib.rs

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,4 +1,27 @@
+use bevy::render::color::Color;
+
 pub mod fps;
 pub mod plugin;
 
 pub const DEFAULT_EXAMPLES_FONT_SIZE: f32 = 17.;
+
+pub const COLORS: &'static [Color] = &[
+    Color::GREEN,
+    Color::BLUE,
+    Color::BLACK,
+    Color::RED,
+    Color::YELLOW,
+    Color::MAROON,
+    Color::PURPLE,
+    Color::SALMON,
+    Color::ORANGE,
+    Color::CYAN,
+    Color::NAVY,
+    Color::OLIVE,
+    Color::PINK,
+    Color::ALICE_BLUE,
+    Color::CRIMSON,
+    Color::TURQUOISE,
+    Color::YELLOW_GREEN,
+    Color::TEAL,
+];
