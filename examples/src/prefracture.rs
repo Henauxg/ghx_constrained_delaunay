f29c00bdc2b932c2a604e0ad62952efd07648f93
H8sh <108821353+H8sh@users.noreply.github.com>
2024-04-12 11:34:28 +0200

    Prefracture example (#1)
    * Enable scenes and gltf features in bevy
    
    * Update prefracture example to load a cube asset
    
    * Enable git lfs for .glb files
    
    * Add cube asset
    
    * Add fragmented test cube asset
    
    * Add rapier 3d dependency in examples
    
    * WIP: Spawn prefaractured frags with physics
    
    * add egui and inspector to example
    
    * add cube frac split and cube frac
    
    * wip: modify example to toggle between frac and not frac cube
    
    * Resolve requested changes

--- examples/src/prefracture.rs
+++ examples/src/prefracture.rs
@@ -1,53 +1,44 @@
-use std::f32::consts::PI;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_rapier3d::prelude::*;
+use std::f32::consts::*;
 
 use bevy::{
-    app::{App, Startup, Update},
-    asset::Assets,
-    core_pipeline::core_3d::Camera3dBundle,
-    ecs::{
-        schedule::IntoSystemConfigs,
-        system::{Commands, ResMut},
-    },
-    input::{common_conditions::input_just_pressed, keyboard::KeyCode},
-    math::{primitives::Plane3d, EulerRot, Quat, Vec3},
-    pbr::{
-        AmbientLight, CascadeShadowConfigBuilder, DirectionalLight, DirectionalLightBundle,
-        PbrBundle, StandardMaterial,
-    },
-    render::{
-        color::Color,
-        mesh::{Mesh, Meshable},
-    },
-    transform::components::Transform,
-    utils::default,
-    DefaultPlugins,
+    input::common_conditions::input_just_pressed, pbr::CascadeShadowConfigBuilder, prelude::*,
 };
 use bevy_ghx_utils::camera::{toggle_auto_orbit, update_pan_orbit_camera, PanOrbitCamera};
 
+pub const CUBE_FRAC_ASSET_PATH: &str = "cube_frac.glb#Scene0";
+pub const CUBE_ASSET_PATH: &str = "cube.glb#Scene0";
+
 fn main() {
-    let mut app = App::new();
-
-    app.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 2000.,
-    })
-    .add_plugins(DefaultPlugins);
-
-    app.add_systems(Startup, (setup_camera, setup_sandbox));
-    app.add_systems(
-        Update,
-        (
-            toggle_auto_orbit.run_if(input_just_pressed(KeyCode::F5)),
-            update_pan_orbit_camera,
-        ),
-    );
-
-    app.run();
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
+        .insert_resource(Time::<Fixed>::from_seconds(10.5))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        // .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Startup, (setup_camera, setup_scene))
+        .add_systems(
+            Update,
+            (
+                toggle_auto_orbit.run_if(input_just_pressed(KeyCode::F5)),
+                update_pan_orbit_camera,
+            ),
+        )
+        .add_systems(
+            Update,
+            (respawn_cube, attach_physics_components_to_cells).chain(),
+        )
+        .run();
 }
 
 pub fn setup_camera(mut commands: Commands) {
     // Camera
-    let camera_position = Vec3::new(0., 1.5, 2.5);
+    let camera_position = Vec3::new(0., 4.5, 7.5);
     let look_target = Vec3::ZERO;
     commands.spawn((
         Camera3dBundle {
@@ -62,18 +53,43 @@ pub fn setup_camera(mut commands: Commands) {
     ));
 }
 
-pub fn setup_sandbox(
+#[derive(Component, Debug)]
+struct FragmentedCubeRoot {
+    physics_applied: bool,
+}
+
+impl Default for FragmentedCubeRoot {
+    fn default() -> Self {
+        Self {
+            physics_applied: false,
+        }
+    }
+}
+
+#[derive(Component)]
+struct ExampleCube;
+
+fn setup_scene(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // // Plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Plane3d::default().mesh().size(500000.0, 500000.0)),
-        transform: Transform::from_xyz(0.0, -0.5, 0.0),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
-        ..default()
-    });
+    let radius = 2000.;
+    let height = 200.;
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cylinder::new(radius, height)),
+            material: materials.add(Color::WHITE),
+            transform: Transform::from_xyz(0.0, -height / 2., 0.0),
+            ..default()
+        },
+        Collider::cylinder(height / 2., radius),
+        (ActiveCollisionTypes::default()),
+        Friction::coefficient(0.7),
+        Restitution::coefficient(0.3),
+    ));
 
     // Light
     commands.spawn(DirectionalLightBundle {
@@ -90,4 +106,82 @@ pub fn setup_sandbox(
         .into(),
         ..default()
     });
+
+    spawn_frac_cube(&asset_server, &mut commands);
+}
+
+fn spawn_cube(asset_server: &Res<AssetServer>, commands: &mut Commands) {
+    commands.spawn((
+        SceneBundle {
+            scene: asset_server.load(CUBE_ASSET_PATH),
+            transform: Transform::from_xyz(0., 2., 0.),
+            ..default()
+        },
+        ExampleCube,
+    ));
+}
+
+fn spawn_frac_cube(asset_server: &Res<AssetServer>, commands: &mut Commands) {
+    commands.spawn((
+        SceneBundle {
+            scene: asset_server.load(CUBE_FRAC_ASSET_PATH),
+            transform: Transform::from_xyz(0., 2., 0.),
+            ..default()
+        },
+        FragmentedCubeRoot::default(),
+        ExampleCube,
+    ));
+}
+
+fn respawn_cube(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    query_cubes: Query<Entity, With<ExampleCube>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+) {
+    if keyboard_input.pressed(KeyCode::KeyF) {
+        for entity in query_cubes.iter() {
+            commands.entity(entity).despawn_recursive();
+        }
+        spawn_cube(&asset_server, &mut commands);
+    }
+    if keyboard_input.pressed(KeyCode::KeyG) {
+        for entity in query_cubes.iter() {
+            commands.entity(entity).despawn_recursive();
+        }
+        spawn_frac_cube(&asset_server, &mut commands);
+    }
+}
+
+fn attach_physics_components_to_cells(
+    mut commands: Commands,
+    mut fractured_scene: Query<(Entity, &mut FragmentedCubeRoot)>,
+    children: Query<&Children>,
+    meshes_handles: Query<&Handle<Mesh>>,
+    meshes: ResMut<Assets<Mesh>>,
+) {
+    for (fractured_scene_entity, mut fragments_root) in fractured_scene.iter_mut() {
+        // We only want to attach the physics components once
+        if !fragments_root.physics_applied {
+            for entity in children.iter_descendants(fractured_scene_entity) {
+                // Attach the physics components only to the meshes entities
+                if let Ok(mesh_handle) = meshes_handles.get(entity) {
+                    let mesh = meshes.get(mesh_handle).unwrap();
+                    info!("Attaching physics components to entity {:?}", entity);
+                    let collider =
+                        Collider::from_bevy_mesh(mesh, &ComputedColliderShape::ConvexHull).unwrap();
+                    commands.entity(entity).insert((
+                        RigidBody::Dynamic,
+                        collider,
+                        ActiveCollisionTypes::default(),
+                        Friction::coefficient(0.7),
+                        Restitution::coefficient(0.05),
+                        ColliderMassProperties::Density(2.0),
+                    ));
+                    // Children do not seem to be created at the same time as the cube root entity, so we only update the flag once children are present
+                    fragments_root.physics_applied = true;
+                }
+            }
+        }
+    }
 }
