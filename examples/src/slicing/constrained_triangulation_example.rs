9784b678ac0e3397c81f4c26528bb37fd2a0810d
Hash <sw.henaux@gmail.com>
2024-05-01 10:43:24 +0200

    constrained triangulation exemple

---
+++ examples/src/slicing/constrained_triangulation_example.rs
@@ -0,0 +1,78 @@
+use bevy::prelude::*;
+
+use examples::plugin::ExamplesPlugin;
+use utils::constrained_triangulation::triangulate_3d_planar_vertices_constrained;
+
+pub mod utils;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, ExamplesPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, draw_triangulation)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut vertices = Vec::<[f32; 3]>::new();
+    vertices.push([0., 8., 0.]);
+    vertices.push([3., 10., 0.]);
+    vertices.push([8., 8., 0.]);
+    vertices.push([5., 5., 0.]);
+    vertices.push([0., 0., 0.]);
+
+    let mut constrained_edges = Vec::new();
+    constrained_edges.push((0 as usize, 2 as usize));
+
+    let indices =
+        triangulate_3d_planar_vertices_constrained(&vertices, Vec3::Z.into(), &constrained_edges);
+
+    commands.insert_resource(DebugVertData { vertices, indices })
+}
+
+#[derive(Resource)]
+struct DebugVertData {
+    vertices: Vec<[f32; 3]>,
+    indices: Vec<usize>,
+}
+
+fn draw_triangulation(mut gizmos: Gizmos, debug_vert_data: Res<DebugVertData>) {
+    for (triangle_id, triangle_indices) in debug_vert_data.indices.chunks_exact(3).enumerate() {
+        let (v1, v2, v3) = (
+            debug_vert_data.vertices[triangle_indices[0]],
+            debug_vert_data.vertices[triangle_indices[1]],
+            debug_vert_data.vertices[triangle_indices[2]],
+        );
+        let color = COLORS[triangle_id % COLORS.len()];
+        gizmos.linestrip(
+            vec![
+                Vec3::from_array(v1),
+                Vec3::from_array(v2),
+                Vec3::from_array(v3),
+                Vec3::from_array(v1),
+            ],
+            color,
+        );
+    }
+}
+
+const COLORS: &'static [Color] = &[
+    Color::GREEN,
+    Color::BLUE,
+    Color::BLACK,
+    Color::RED,
+    Color::YELLOW,
+    Color::MAROON,
+    Color::PURPLE,
+    Color::SALMON,
+    Color::ORANGE,
+    Color::CYAN,
+    Color::NAVY,
+    Color::OLIVE,
+    Color::PINK,
+    Color::ALICE_BLUE,
+    Color::CRIMSON,
+    Color::TURQUOISE,
+    Color::YELLOW_GREEN,
+    Color::TEAL,
+];
