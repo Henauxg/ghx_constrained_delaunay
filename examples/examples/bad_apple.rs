a7991c9f21f22d5fe27887248578f42797186129
Henauxg <19689618+Henauxg@users.noreply.github.com>
2024-07-18 20:40:48 +0200

    examples(bad_appl)e: update
    Former-commit-id: dc14693e3803d6446452b7097c8f3119d2db066e

--- examples/examples/bad_apple.rs
+++ examples/examples/bad_apple.rs
@@ -4,18 +4,20 @@ use bevy::{
     app::{App, Startup, Update},
     core_pipeline::{bloom::BloomSettings, tonemapping::Tonemapping},
     ecs::system::{Commands, Res},
+    input::{common_conditions::input_just_pressed, ButtonInput},
+    log::info,
     math::Vec3,
-    prelude::{Camera3dBundle, EventWriter, ResMut, Resource},
+    prelude::{Camera3dBundle, EventWriter, IntoSystemConfigs, KeyCode, ResMut, Resource},
     render::camera::Camera,
     time::{Time, Timer, TimerMode},
     transform::components::Transform,
     utils::default,
     DefaultPlugins,
 };
-use bevy_ghx_utils::camera::{update_pan_orbit_camera, PanOrbitCamera};
 use examples::{
-    LabelMode, TriangleDebugCursorUpdate, TriangleDebugPlugin, TrianglesDebugData,
-    TrianglesDebugViewConfig, TrianglesDrawMode, VertexLabelMode,
+    camera::{update_pan_orbit_camera, PanOrbitCamera},
+    update_triangles_debug_entities, LabelMode, TriangleDebugCursorUpdate, TriangleDebugPlugin,
+    TrianglesDebugData, TrianglesDebugViewConfig, TrianglesDrawMode, VertexLabelMode,
 };
 use ghx_constrained_delaunay::{
     constrained_triangulation::ConstrainedTriangulationConfiguration,
@@ -26,9 +28,10 @@ use ghx_constrained_delaunay::{
 use serde::Deserialize;
 
 const FRAMES_FILE: &str = "./assets/bad_apple_frames.msgpack";
-const DEFAULT_FRAME_DISPLAY_PERIOD_MS: u64 = 30;
+const DEFAULT_FRAME_DISPLAY_PERIOD_MICROS: u64 = 29850;
 const DEFAULT_FRAME_SWITCH_MODE: FrameSwitchMode = FrameSwitchMode::Auto;
 
+#[derive(Debug)]
 pub enum FrameSwitchMode {
     Manual,
     Auto,
@@ -38,8 +41,14 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, TriangleDebugPlugin))
         .add_systems(Startup, (setup, setup_camera))
-        .add_systems(Update, (update_pan_orbit_camera, next_frame))
-        // .add_systems(Update, draw_origin_circle)
+        .add_systems(
+            Update,
+            (
+                update_pan_orbit_camera,
+                switch_frame_mode.run_if(input_just_pressed(KeyCode::Space)),
+                next_frame.before(update_triangles_debug_entities),
+            ),
+        )
         .run();
 }
 
@@ -59,9 +68,12 @@ pub fn setup_camera(mut commands: Commands) {
         },
         BloomSettings::NATURAL,
         PanOrbitCamera {
-            radius: (look_target - camera_position).length(),
+            focus: Vec3::new(240.02797, -182.32127, 0.0),
+            radius: 475.5253,
+            upside_down: false,
             auto_orbit: false,
-            ..Default::default()
+            auto_orbit_factor: 0.3,
+            needs_transform_refresh: true,
         },
     ));
 }
@@ -87,8 +99,11 @@ pub struct Frames {
 }
 
 fn setup(mut commands: Commands) {
+    info!("Deserializing frames from file...");
     let frames_bytes = fs::read(FRAMES_FILE).expect("Should have been able to load frames file");
     let frames: Vec<Frame> = rmp_serde::from_slice(&frames_bytes).unwrap();
+
+    info!("Triangulating {} frames...", frames.len());
 
     let config = ConstrainedTriangulationConfiguration {
         debug_config: DebugConfiguration {
@@ -103,7 +118,7 @@ fn setup(mut commands: Commands) {
         // if i != 897 {
         //     continue;
         // }
-        // info!("Loading frame n°{}", i);
+        // info!("Loading frame n°{}", _i);
         let edges = frame
             .edges
             .iter()
@@ -130,7 +145,7 @@ fn setup(mut commands: Commands) {
         });
     }
 
-    commands.insert_resource(TrianglesDebugData::new_with_constraintss(
+    commands.insert_resource(TrianglesDebugData::new_with_constraints(
         triangulated_frames[0].displayed_vertices.clone(),
         &triangulated_frames[0].edges,
         triangulated_frames[0].triangulation.debug_context.clone(),
@@ -140,40 +155,74 @@ fn setup(mut commands: Commands) {
         LabelMode::None,
         VertexLabelMode::GlobalIndex,
         TrianglesDrawMode::AllAsContourAndInteriorMeshes,
+        false,
     ));
 
     commands.insert_resource(Frames {
         frame_index: 0,
         next_frame_timer: Timer::new(
-            Duration::from_millis(DEFAULT_FRAME_DISPLAY_PERIOD_MS),
+            Duration::from_micros(DEFAULT_FRAME_DISPLAY_PERIOD_MICROS),
             TimerMode::Repeating,
         ),
         switch_mode: DEFAULT_FRAME_SWITCH_MODE,
         triangulated_frames,
     });
+
+    info!("All {} frames are loaded and ready", frames.len());
 }
 
 fn next_frame(
     mut commands: Commands,
     time: Res<Time>,
     mut frames: ResMut<Frames>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut debug_data_updates_events: EventWriter<TriangleDebugCursorUpdate>,
 ) {
+    let mut updated = false;
     match frames.switch_mode {
-        FrameSwitchMode::Manual => (),
+        FrameSwitchMode::Manual => {
+            if keyboard_input.just_pressed(KeyCode::ArrowLeft)
+                || keyboard_input.pressed(KeyCode::ArrowDown)
+            {
+                if frames.frame_index == 0 {
+                    frames.frame_index = frames.triangulated_frames.len() - 1;
+                } else {
+                    frames.frame_index -= 1;
+                }
+                updated = true;
+                info!("Frame n°{}", frames.frame_index);
+            } else if keyboard_input.just_pressed(KeyCode::ArrowRight)
+                || keyboard_input.pressed(KeyCode::ArrowUp)
+            {
+                frames.frame_index = (frames.frame_index + 1) % frames.triangulated_frames.len();
+                updated = true;
+                info!("Frame n°{}", frames.frame_index);
+            }
+        }
         FrameSwitchMode::Auto => {
             frames.next_frame_timer.tick(time.delta());
             if frames.next_frame_timer.finished() {
                 frames.frame_index = (frames.frame_index + 1) % frames.triangulated_frames.len();
-
-                let frame_data = &frames.triangulated_frames[frames.frame_index];
-                commands.insert_resource(TrianglesDebugData::new_with_constraintss(
-                    frame_data.displayed_vertices.clone(),
-                    &frame_data.edges,
-                    frame_data.triangulation.debug_context.clone(),
-                ));
-                debug_data_updates_events.send(TriangleDebugCursorUpdate);
+                updated = true;
             }
         }
     }
+
+    if updated {
+        let frame_data = &frames.triangulated_frames[frames.frame_index];
+        commands.insert_resource(TrianglesDebugData::new_with_constraints(
+            frame_data.displayed_vertices.clone(),
+            &frame_data.edges,
+            frame_data.triangulation.debug_context.clone(),
+        ));
+        debug_data_updates_events.send(TriangleDebugCursorUpdate);
+    }
+}
+
+pub fn switch_frame_mode(mut frames: ResMut<Frames>) {
+    frames.switch_mode = match frames.switch_mode {
+        FrameSwitchMode::Manual => FrameSwitchMode::Auto,
+        FrameSwitchMode::Auto => FrameSwitchMode::Manual,
+    };
+    info!("Frame switch mode set to {:?}", frames.switch_mode);
 }
